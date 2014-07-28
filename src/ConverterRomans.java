9e229d4c95001ae4e65fd81b2e5af93b0c38bc09
Przemek Kura <kuraprzemek@gmail.com>
2014-07-28 13:38:53 +0200

    add new diabolical test for 10 - test passed (but code smell to many if => refacoting)

--- src/ConverterRomans.java
+++ src/ConverterRomans.java
@@ -4,6 +4,7 @@ public class ConverterRomans {
 	public int convert(String romansNum) {
 		if(romansNum == "I") return 1;
 		else if(romansNum == "V") return 5;
+		else if(romansNum == "X") return 10;
 		
 		return 0;
 	}
