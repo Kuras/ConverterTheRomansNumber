b82edbdfa72525cc3e88aaf8e460feb7d977fe23
Przemek Kura <kuraprzemek@gmail.com>
2014-07-28 13:42:16 +0200

    [refactoing] small step => test works

--- src/ConverterRomans.java
+++ src/ConverterRomans.java
@@ -1,11 +1,19 @@
+import java.util.HashMap;
+
 
 public class ConverterRomans {
+
+	private static HashMap<String, Integer> table;
+	
+	public ConverterRomans() {
+		table = new HashMap<String,Integer>();
+	}
 
 	public int convert(String romansNum) {
 		if(romansNum == "I") return 1;
 		else if(romansNum == "V") return 5;
 		else if(romansNum == "X") return 10;
-		
+			
 		return 0;
 	}
 
