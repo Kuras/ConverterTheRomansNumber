3f5c35550530aff57a4b967a6683a9639a8fd684
Przemek Kura <kuraprzemek@gmail.com>
2014-07-28 13:53:55 +0200

    change data abstract type but if we have redundation that dont be work

--- src/ConverterRomans.java
+++ src/ConverterRomans.java
@@ -6,16 +6,16 @@ public class ConverterRomans {
 	private static HashMap<String, Integer> table;
 	
 	public ConverterRomans() {
-		table = new HashMap<String,Integer>();
-		table.put("I", 1);
-		table.put("II", 2);
-		table.put("III", 3);
-		table.put("V", 5);
-		table.put("X", 10);
-		table.put("L", 50);
-		table.put("C", 100);
-		table.put("D", 500);
-		table.put("M", 1000);
+		table = new HashMap<Character,Integer>();
+		table.put('I', 1);
+		table.put('I', 2);
+		table.put('III', 3);
+		table.put('V', 5);
+		table.put('X', 10);
+		table.put('L', 50);
+		table.put('C', 100);
+		table.put('D', 500);
+		table.put('M', 1000);
 	}
 
 	public int convert(String romansNum) {
