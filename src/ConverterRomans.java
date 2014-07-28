8c6f30a0e1e55ac9343f5f1f6a5ce375fe1a530b
Przemek Kura <kuraprzemek@gmail.com>
2014-07-28 13:55:33 +0200

    yeah all test work

--- src/ConverterRomans.java
+++ src/ConverterRomans.java
@@ -3,13 +3,11 @@ import java.util.HashMap;
 
 public class ConverterRomans {
 
-	private static HashMap<String, Integer> table;
+	private static HashMap<Character,Integer> table;
 	
 	public ConverterRomans() {
 		table = new HashMap<Character,Integer>();
 		table.put('I', 1);
-		table.put('I', 2);
-		table.put('III', 3);
 		table.put('V', 5);
 		table.put('X', 10);
 		table.put('L', 50);
@@ -20,7 +18,7 @@ public class ConverterRomans {
 
 	public int convert(String romansNum) {
 		int summary=0;
-		for (int i = 0; i < table.size(); i++) {
+		for (int i = 0; i < romansNum.length(); i++) {
 			summary += table.get(romansNum.charAt(i));
 		}
 		return summary;
