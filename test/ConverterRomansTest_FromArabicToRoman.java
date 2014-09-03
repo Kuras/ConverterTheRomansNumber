5cd3e416c93187724d942c4507e5a16d988bf28a
Przemek Kura <kuraprzemek@gmail.com>
2014-09-03 20:18:25 +0200

    add new test file for new functionality

---
+++ test/ConverterRomansTest_FromArabicToRoman.java
@@ -0,0 +1,28 @@
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+
+public class ConverterRomansTest_FromArabicToRoman {
+
+	private ConverterRomans converterRomans;
+	
+	@Before
+	public void setUp() throws Exception {
+		converterRomans = new ConverterRomans();
+	}
+	
+	@After
+	public void after() throws Exception {
+		converterRomans = null;
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+		
+	}
+
+}
